76bbf73db2d9fed50310d1db5203ba29e5eef586
Rick Yiu <rickyiu@google.com>
2019-10-05 15:59:06 +0800

    sched: use sysctl to control energy aware feature
    Energy aware feature control is previously done through debugfs,
    which will be deprecated, so move the control to sysctl.
    
    [B--B: backported to 4.4 kernel]
    
    Bug: 141333728
    Test: function works as expected
    Change-Id: I55411d3bb2669ba1fae3225d67cdf1cf8b3b3a7f
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -92,6 +92,11 @@ static unsigned int sched_nr_latency = 6;
 unsigned int sysctl_sched_child_runs_first __read_mostly = 1;
 
 /*
+ * To enable/disable energy aware feature.
+ */
+unsigned int __read_mostly sysctl_sched_energy_aware = 1;
+
+/*
  * SCHED_OTHER wake-up granularity.
  * (default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
  *
@@ -5192,6 +5197,11 @@ static void record_wakee(struct task_struct *p)
 	}
 }
 
+inline bool energy_aware(void)
+{
+	return sysctl_sched_energy_aware;
+}
+
 static void task_waking_fair(struct task_struct *p)
 {
 	struct sched_entity *se = &p->se;
@@ -5370,11 +5380,6 @@ unsigned long capacity_min_of(int cpu)
 	return arch_scale_cpu_capacity(NULL, cpu) *
 	       arch_scale_min_freq_capacity(NULL, cpu)
 	       >> SCHED_CAPACITY_SHIFT;
-}
-
-static inline bool energy_aware(void)
-{
-       return sched_feat(ENERGY_AWARE);
 }
 
 /*
